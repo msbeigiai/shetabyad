1f799f7c9f8524101033448ee8e3e6e5efa1c1e5
Mohsen Sadeghbeigi <mohsen.sadegh62@gmail.com>
2017-05-09 11:12:55 +0430

    Solved rails admin issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users, :admin_user
+  mount RailsAdmin::Engine => '/user_admin', as: 'rails_admin'
+  devise_for :users
+  devise_for :admins, path: 'admin'
   
   resources :lessons
   root 'welcome#index'
