eb580941ac2afcca18fcb7ea5dec910bc39520d0
Mohsen Sadeghbeigi <mohsen.sadegh62@gmail.com>
2017-05-09 12:19:27 +0430

    Added markdown and pygments

--- Gemfile
+++ Gemfile
@@ -40,6 +40,8 @@ gem 'devise'
 gem 'carrierwave'
 gem 'font-awesome-sass'
 gem 'rails_admin'
+gem 'redcarpet'
+gem 'pygments.rb'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,26 @@ module ApplicationHelper
   def find_first
     @tutorial.lessons.first 
   end
+
+  class HTMLWithPygments < Redcarpet::Render::HTML
+    def block_code(code, language)
+      Pygments.highlight(code, lexer: language)
+    end
+  end
+
+  def markdown(text)
+    renderer = HTMLWithPygments.new(hard_wrap: true, filter_html: true)
+    options = {
+      autolink: true, 
+      no_intra_emphasis: true, 
+      fenced_code_blocks: true, 
+      strikethrough: true,
+      superscript: true,
+      lax_html_blocks: true
+    }
+    marked = Redcarpet::Markdown.new(renderer, options)
+    marked.render(text).html_safe
+  end
+  
   
 end
