b496c0433fd2b35240462fa06441c43f59cb60de
Mohsen Sadeghbeigi <mohsen.sadegh62@gmail.com>
2017-05-21 10:20:02 +0430

    Added username to devise and working on styles

--- app/assets/javascripts/loadMoreComments.js
+++ app/assets/javascripts/loadMoreComments.js
@@ -1,20 +1,20 @@
 var Append = {};
 Append.open = false;
-function ClickableCommentsLink(){
+function ClickableCommentsLink() {
   $('.more-comments').click( function() {
-    $(this).on('ajax:success', function(event, data, status,xhr) {
+    $(this).on('ajax:success', function(event, data, status, xhr) {
       event.preventDefault();
       var lessonId = $(this).data("lesson-id");
       $("#comments_" + lessonId).html(data);
       $("#comments-paginator-" + lessonId).html("<a id='more-comments' data-lesson-id=" + lessonId + "data-type='html' data-remote='true' href='/lessons/" + lessonId + "/comments>show more comments</a>");
       Append.open = true;
-      Append.comment = true; 
-      Append.link = false;  
-    }); 
+      Append.comment = true;
+      Append.link = false;
+    });
   });
 }
 
-function DestroyComments(){
+function DestroyComments() {
   $('.delete-comment').click( function() {
     Append.id = this;
     Append.lesson_id = $(this).data("lesson-id");
@@ -22,15 +22,15 @@ function DestroyComments(){
   }); 
 }
 
-$( document ).ready(function() {
+$( document ).on('turbolinks:load', function() {
   ClickableCommentsLink();
   DestroyComments();
-  $('.comment_content').click (function(){
+  $('.comment_content').click (function() {
   	Append.id = this;
   	Append.lesson_id = $(this).data("lesson-id");
   	if (Append.comment_count == undefined){ Append.comment_count = $(this).data("value"); }
-  	if(Append.comment_count < 4){ Append.comment = true; Append.link = false; } 
-  	else if(Append.comment_count == 4){ Append.comment = false; Append.link = true; } 
-  	else if(Append.comment_count > 4){ Append.comment = false; Append.link = false;  } 
-  })
+  	if(Append.comment_count < 4){ Append.comment = true; Append.link = false; }
+  	else if(Append.comment_count == 4) { Append.comment = false; Append.link = true; }
+  	else if(Append.comment_count > 4) { Append.comment = false; Append.link = false;  }
+  });
 });
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517123820) do
+ActiveRecord::Schema.define(version: 20170521044233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,8 +78,10 @@ ActiveRecord::Schema.define(version: 20170517123820) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
   create_table "videos", force: :cascade do |t|
