566c06934fa5899ba53ceb2f0efece249fa9e382
Mohsen Sadeghbeigi <mohsen.sadegh62@gmail.com>
2017-06-10 15:50:40 +0430

    Added some styling and footer editing

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,4 +3,6 @@ class Blog < ApplicationRecord
   friendly_id :title_url, use: :slugged
   
   mount_uploader :picture, ImageUploader
+  
+  belongs_to :admin
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610063957) do
+ActiveRecord::Schema.define(version: 20170610070210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170610063957) do
     t.string   "picture"
     t.string   "title_url"
     t.string   "slug"
+    t.integer  "admin_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
   end
 
